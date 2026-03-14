960b049a9b65dda6d3b3054b649d67d5198657aa
codewithstella <coderluffy35@gmail.com>
2026-03-14 08:36:18 +0530

    chapter 01 project setup and database

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+import { prisma } from "@/lib/db";
+
+export default async function TestPage() {
+    const voices = await prisma.voice.findMany();
+
+    return (<div>
+            <h1>Voices Page {voices.length}</h1>
+            <ul>
+                {voices.map((voice) => (
+                    <li key={voice.id}>{voice.name} - {voice.variant}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
